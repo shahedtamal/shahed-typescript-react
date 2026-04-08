d241e8a2bd86c08d8bc3163ed89b2b231cbb782f
shahedtamal <shahedtamal@gmail.com>
2026-04-08 17:57:36 +0900

    added random word logic and console log

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-
+import { useState } from "react";
+import words from "./wordList.json"
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(()=> {
+    return words[Math.floor(Math.random()*words.length)]
+  })
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
   
-  return(
-  <h1>Hi</h1>
-  );
-}
+  return 
+    <div style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection:"column",
+        gap:"2rem",
+        margin:"0 auto"
+        alignItems:"center",
+}}
+>
+  <div style={{fontSize:"2rem",txtAlign:"center"}}>Lose Win</div>
+  </div>
 
-export default App;
+export default App
